3b7664e33f62cdfe65c22115b948c7a8998bd2b3
jaymondyu <18875001129@163.com>
2019-07-05 17:40:07 +0800

    22
    22

--- spiderman_modle/clear.py
+++ spiderman_modle/clear.py
@@ -2,11 +2,11 @@ import re
 import urllib.request
 from urllib import parse
 
-f = open('modle_data.json','r')
+f = open('modle_data.json','r',encoding='utf-8')
 data = f.read()
 
-all = re.findall(r'{.*?}', data,re.S)[0:20]
-all_1 = urllib.parse.urlencode(all).encode('utf-8')
+all = re.findall(r'{.*?}', data,re.S)
 
-with open('clear_data.txt','w') as w:
-    w.write(str(all_1))
+
+with open('clear_data.txt','w',encoding='utf-8') as w:
+    w.write(str(all))
