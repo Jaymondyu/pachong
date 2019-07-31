a7f61fe6f9a230bb0a45f9b9d0f06b5c49e91bf6
Jialun Yu <yujialun@tylin.com.cn>
2019-07-31 17:46:09 +0800

    22
    22

--- for_weather/spider.py
+++ for_weather/spider.py
@@ -6,45 +6,37 @@ import flask
 
 app = flask.Flask(__name__)
 
-@app.route("/")
-def weather_hours():
+@app.route("/shenzhen")
+def shenzhen_hours():
 
-    day = []
 
-    url = 'http://www.weather.com.cn/weather1d/101290101.shtml'
+    url = 'https://wis.qq.com/weather/common?source=pc&weather_type=observe%7Cforecast_1h%7Cforecast_24h%7Cindex%7Calarm%7Climit%7Ctips%7Crise&province=%E5%B9%BF%E4%B8%9C&city=%E6%B7%B1%E5%9C%B3&county=&callback=jQuery1113035920872837971163_1564533727240&_=1564533727245'
 
     headers={
-        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36",
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36"
     }
-
-    # request = urllib.request.Request(url,headers=headers)
 
     response = requests.get(url)
 
     data = response.text
 
-    data_weather = re.findall(r"\"7d\":\[(.*?)]}",data,re.S)
+    data = json.loads(data[data.find('{'):-1])
+
+    world = data["data"]["forecast_1h"]
 
 
-    # print(json.loads("["+data_weather[0]+"]")[0])
 
-    for i in json.loads("["+data_weather[0]+"]"):
-        for l in i:
-            arr = l.split(',')
-            day.append([arr[0],arr[2],arr[3]])
-
-    hours = day[:8]
+    return json.dumps(world)
 
 
-    return json.dumps(hours)
 
-@app.route("/7d")
-def weather_qq():
+@app.route("/shenzhen/7d")
+def shenzhen_7d():
 
     url = 'https://wis.qq.com/weather/common?source=pc&weather_type=observe%7Cforecast_1h%7Cforecast_24h%7Cindex%7Calarm%7Climit%7Ctips%7Crise&province=%E5%B9%BF%E4%B8%9C&city=%E6%B7%B1%E5%9C%B3&county=&callback=jQuery111307645707250197813_1564455732836&_=1564455732841'
 
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36",
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36"
     }
 
     response = requests.get(url)
@@ -56,26 +48,86 @@ def weather_qq():
 
     hello = data["data"]["forecast_24h"]
 
-    rainNum = 0
-    tfNum = 0
+    # rainNum = 0
+    # tfNum = 0
+    # High = 0
+    # Low = 0
+    # for m in range(1, 8):
+    #     if (str(hello[str(m)]).find("雨") != -1):
+    #         rainNum += 1
+    #     if (str(hello[str(m)]).find("台风") != -1):
+    #         tfNum += 1
+    #     if int(hello[str(m)]["max_degree"]) > 35:
+    #         High += 1
+    #     if int(hello[str(m)]["min_degree"]) < 5:
+    #         Low += 1
+    #
+    # Tea = [rainNum, tfNum, High, Low]
+
+
+
+    return json.dumps(hello)
+
+
+
+@app.route("/shenzhen/alarm")
+def shenzhen_alarm():
+
+    url = 'https://wis.qq.com/weather/common?source=pc&weather_type=observe%7Cforecast_1h%7Cforecast_24h%7Cindex%7Calarm%7Climit%7Ctips%7Crise&province=%E5%B9%BF%E4%B8%9C&city=%E6%B7%B1%E5%9C%B3&county=&callback=jQuery1113035920872837971163_1564533727240&_=1564533727245'
+
+    headers={
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36"
+    }
+
+    response = requests.get(url)
+
+    data = response.text
+
+    data = json.loads(data[data.find('{'):-1])
+
+    warning = data["data"]["alarm"]
+
+
+
+    url_1 = 'https://wis.qq.com/weather/common?source=pc&weather_type=observe%7Cforecast_1h%7Cforecast_24h%7Cindex%7Calarm%7Climit%7Ctips%7Crise&province=%E5%B9%BF%E4%B8%9C&city=%E6%B7%B1%E5%9C%B3&county=&callback=jQuery111307645707250197813_1564455732836&_=1564455732841'
+
+
+
+    response_1 = requests.get(url_1)
+
+    data_1 = response_1.text
+
+    data_1 = json.loads(data_1[data_1.find('{'):-1])
+
+
+    hello_1 = data["data"]["forecast_24h"]
+
     High = 0
-    Low = 0
+    Rain = 0
+
     for m in range(1, 8):
-        if (str(hello[str(m)]).find("雨") != -1):
-            rainNum += 1
-        if (str(hello[str(m)]).find("台风") != -1):
-            tfNum += 1
-        if int(hello[str(m)]["max_degree"]) > 35:
+        if int(hello_1[str(m)]["max_degree"]) > 35:
             High += 1
-        if int(hello[str(m)]["min_degree"]) < 5:
-            Low += 1
-
-    Tea = [rainNum, tfNum, High, Low]
-
-    final = [hello, Tea]
+        if (str(hello_1[str(m)]).find("\u96e8") != -1):
+            Rain += 1
 
 
-    return json.dumps(final)
+    if (str(warning).find("\u53f0\u98ce") != -1):
+        tf = 1
+    else:
+        tf = 0
+
+
+    if (str(warning).find("\u96f7\u7535") != -1):
+        ld = 1
+    else:
+        ld = 0
+
+    alarm=[tf,ld,Rain,High]
+
+    return json.dumps(alarm)
+
+
 
 
 application = app
